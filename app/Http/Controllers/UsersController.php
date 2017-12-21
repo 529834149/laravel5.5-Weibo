f3ea992ff09e4f59c167e701e5ecc4221fc11a64
chenbaojin <529834149@qq.com>
2017-12-21 10:17:08 +0800

    增加用户个信息及修改个人资料功能模块 涉及到语言包、requests表单提交验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;//表单请求验证（FormRequest）
 class UsersController extends Controller
 {
     /**
@@ -54,9 +55,9 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(User $user)
     {
-        //
+        return view('users.edit', compact('user'));
     }
 
     /**
@@ -65,10 +66,12 @@ class UsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
+     * UserRequest 表单请求验证（FormRequest）
      */
-    public function update(Request $request, $id)
+    public function update(UserRequest $request, User $user)
     {
-        //
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 
     /**
